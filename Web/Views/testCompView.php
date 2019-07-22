b8998a0615e30dade65a483542f99215fcc593df
Christian Hasieber <HasiCh@skd.semikron.com>
2019-07-22 14:05:56 +0200

    Datenbankanbindung zur Testdatenbank

--- Web/Views/testCompView.php
+++ Web/Views/testCompView.php
@@ -7,7 +7,10 @@
 ?>
 
 <?php echo $demoComp['comp_Id']." ";
-    echo  $demoComp['comp_Designation']?>
+	echo  $demoComp['comp_Designation']
+	
+	
+	?>
 
 <?php
 	// Das hier muss in jeder View eingetragen werden.
