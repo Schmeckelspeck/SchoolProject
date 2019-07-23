a499c1b00388e1fbece9bd18ba20cfcd05683ec0
olifran <Ethereal_No1@web.de>
2019-07-23 08:57:43 +0200

    Aktueller Stand: Nach Besprechung zu Web

--- Datalayer/ComponentDatenAbruf.php
+++ Datalayer/ComponentDatenAbruf.php
@@ -15,7 +15,8 @@
 			'supl_name',
 			'room_description',
 			'room_number',
-			'coty_name'
+			'coty_name',
+			'comp_id'
 		FROM component
 		INNER JOIN component_type ON component_type.coty_id = component.coty_id
 		INNER JOIN room ON room.room_id = component.room_id
@@ -35,6 +36,27 @@
 			'room_description'=>'Computer-Raum',
 			'room_number'=>'1234',
 			'coty_name'=>'CPU'
+		);
+		return $result;
+	}
+
+	function GetComponentAttributes($id)
+	{
+		$sqlStatement = 
+		"SELECT  
+			coat_name 
+		FROM component_attribute
+		INNER JOIN comp_coat ON comp_coat.coca_id = component_attribute.coca_coat_id
+		INNER JOIN component ON component.comp_id = coca_comp_id
+		WHERE component.comp_id = ".$id.";";
+
+		$result = ExecuteReaderAssoc($sqlStatement, 'testDb');
+		$result = array(
+			array('coat_name'=>'Ein Attribut1'),
+			array('coat_name'=>'Ein Attribut2'),
+			array('coat_name'=>'Ein Attribut3'),
+			array('coat_name'=>'Ein Attribut4'),
+			array('coat_name'=>'Ein Attribut5'),
 		);
 		return $result;
 	}
