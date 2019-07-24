7bbfeb32c2dd21253f1c64de9b06a7e8fc95bdcd
RitterDerKokosnuss <f-ritter2012@hotmail.de>
2019-07-24 12:53:55 +0200

    detailsSupplierView erstellt, supplierView 'speichern' - Button eingefügt

---
+++ Web/Views/detailsSupplierView.php
@@ -0,0 +1,71 @@
+<?php
+	// Das hier muss in jeder View eingetragen werden.
+	require_once("../header.php");
+
+	// Das hier muss jeweils bei der View angepasst werden.
+	require_once("../Controllers/BEZEICHNUNGController.php");
+	require_once("../Controllers/ComponentsController.php");
+
+?>
+
+<?php 
+/**<input type='text' value='<?php echo($componentData['comp_description']) ?>'>
+                    <input type='text' value='<?php echo($componentData['coty_name']) ?>'>
+                    <input type='text' value='<?php echo($componentData['comp_manufacturer']) ?>' >
+                     */
+
+echo "<section>
+    <div class='container'>
+
+        <div class='row'>
+        <h2>Komponenten - Details</h2><br><br><br>
+        </div>
+
+        <div class='row'>    
+            <form>
+                <div class='row' >
+      
+                <div class='col'>
+                    <label>Firma</label><br>
+                    <label>Straße</label><br>
+                    <label>PLZ</label><br>
+                    <label>Stadt</label><br>
+                    <label>Telefon</label><br>
+                    <label>Mobil</label><br>
+                    <label>Fax</label><br>
+                    <label>Mail</label>
+                </div>
+
+
+                <div class='col'>
+                    <input type='text' name='firma'><br>
+                    <input type='text' name='strasse'><br>
+                    <input type='text' name='plz'><br>
+                    <input type='text' name='stadt'><br>
+                    <input type='text' name='telefon'><br>
+                    <input type='text' name='mobil'><br>
+                    <input type='text' name='fax'><br>
+                    <input type='text' name='mail'>    
+                </div>
+
+            </div>
+            <br><br>
+            <div class='row' >
+                
+                <br><br>
+                <div class='row'>
+                    <div class='col' >
+                        <button type='button' class='btn btn-dark'>Speichern</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>";
+
+?>
+
+<?php
+	// Das hier muss in jeder View eingetragen werden.
+	require_once("../footer.php");
+?>
--- Web/Views/supplierView.php
+++ Web/Views/supplierView.php
@@ -9,48 +9,41 @@
 ?>
 
 <?php 
-		if(sizeof($allComponents > 0) && $allComponents != NULL)
+		echo "<table class='table'>
+		<thead>
+			<tr>
+			<th scope='col'>Firma</th>
+			<th scope='col'>Strasse</th>
+			<th scope='col'>Ort</th>
+			<th scope='col'>PLZ</th>
+			<th scope='col'>Tel</th>
+			<th scope='col'>E-mail</th>
+			</tr>
+		</thead>";
+		echo "<tbody>";
+
+		foreach($allComponents as $component)
 		{
-			echo "<table class='table'>
-			<thead>
-				<tr>
-				<th scope='col'>Firma</th>
-				<th scope='col'>Strasse</th>
-				<th scope='col'>Ort</th>
-				<th scope='col'>PLZ</th>
-				<th scope='col'>Tel</th>
-				<th scope='col'>E-mail</th>
-				</tr>
-			</thead>";
-			echo "<tbody>";
 
-			foreach($allComponents as $component)
-			{
+			//
+			//Hier müssen noch die Attribute angepasst werden
 
-				//
-				//Hier müssen noch die Attribute angepasst werden
-
-				echo "<tr>";
-				echo "<th scope='row'>".$component['comp_id']."</th>";
-				echo "<td><a href='ComponentView.php?idComponent=".$component['comp_id']."'>".$component['comp_description']."</a></td>";
-				echo "<td>".$component['comp_warranty_length']."</td>";
-				echo "<td>".$component['coty_name']."</td>";
-				echo "<td>".$component['room_description']." (Raum-Nr.: ".$component['room_number'].")</td>";
-				echo "</tr>";
-				
-			}
-			echo "</tbody>";
-			echo "</table>";
+			echo "<tr>";
+			echo "<th scope='row'>".$component['comp_id']."</th>";
+			echo "<td><a href='ComponentView.php?idComponent=".$component['comp_id']."'>".$component['comp_description']."</a></td>";
+			echo "<td>".$component['comp_warranty_length']."</td>";
+			echo "<td>".$component['coty_name']."</td>";
+			echo "<td>".$component['room_description']." (Raum-Nr.: ".$component['room_number'].")</td>";
+			echo "</tr>";
+			
 		}
-		else
-		{
-			echo "Es konnten keine Daten ermittelt werden.";
-		}
+		echo "</tbody>";
+		echo "</table>";
+	
+		echo "<div class='col'>
+			<button type='button' class='btn btn-dark'>Lieranten hinzufügen</button>
+		</div>";
 	?>
-
-
-<br>
-<?php echo "<br>Hier folgt die Variable \$data aus dem Controller:".$data; ?>
 
 <?php
 	// Das hier muss in jeder View eingetragen werden.
